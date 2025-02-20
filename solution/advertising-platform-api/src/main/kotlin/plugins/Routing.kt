00cbbe7454d8ceb25039f1ed477d4f0a5ad238dc
Shaykhraziev Bulat <mrcookie209@gmail.com>
2025-02-20 22:47:25 +0300

    deleted prefix

--- solution/advertising-platform-api/src/main/kotlin/plugins/Routing.kt
+++ solution/advertising-platform-api/src/main/kotlin/plugins/Routing.kt
@@ -6,103 +6,102 @@ import ru.cwshbr.controller.*
 
 fun Application.configureRouting() {
     routing {
-        route("/api") {
-            post("/time/advance"){
-                TimeController(call).incrementTime()
-            }
+        post("/time/advance") {
+            TimeController(call).incrementTime()
+        }
 
-            route("/clients"){
-                post("/bulk"){
-                    ClientController(call).createClients()
-                }
-                get("/{clientId}"){
-                    ClientController(call).getClientById()
-                }
+        route("/clients") {
+            post("/bulk") {
+                ClientController(call).createClients()
             }
+            get("/{clientId}") {
+                ClientController(call).getClientById()
+            }
+        }
 
-            route("/advertisers"){
-                route("/{advertiserId}"){
-                    get{
-                        AdvertiserController(call).getAdvertiser()
+        route("/advertisers") {
+            route("/{advertiserId}") {
+                get {
+                    AdvertiserController(call).getAdvertiser()
+                }
+
+                route("/campaigns") {
+                    post {
+                        CampaignController(call).createCampaign()
+                    }
+                    get {
+                        CampaignController(call).getListOfCampaigns()
                     }
 
-                    route("/campaigns"){
-                        post {
-                            CampaignController(call).createCampaign()
+                    route("/{campaignId}") {
+                        route("/image") {
+                            get {
+                                ImagesController(call).getImage()
+                            }
+                            post {
+                                ImagesController(call).setImage()
+                            }
                         }
                         get {
-                            CampaignController(call).getListOfCampaigns()
+                            CampaignController(call).getCampaignById()
                         }
-
-                        route("/{campaignId}"){
-                            route("/image"){
-                                get{
-                                    ImagesController(call).getImage()
-                                }
-                                post {
-                                    ImagesController(call).setImage()
-                                }
-                            }
-                            get {
-                                CampaignController(call).getCampaignById()
-                            }
-                            put {
-                                CampaignController(call).updateCampaign()
-                            }
-                            delete {
-                                CampaignController(call).deleteCampaign()
-                            }
+                        put {
+                            CampaignController(call).updateCampaign()
+                        }
+                        delete {
+                            CampaignController(call).deleteCampaign()
                         }
                     }
                 }
-
-                post("/bulk") {
-                    AdvertiserController(call).createAdvertisers()
-                }
             }
 
-            post("/ml-scores"){
-                AdvertiserController(call).updateMlScore()
+            post("/bulk") {
+                AdvertiserController(call).createAdvertisers()
             }
-
-            route("/ads") {
-                get{
-                    CampaignController(call).getAd()
-                }
-                post("/{adId}/click") {
-                    CampaignController(call).clickAd()
-                }
-            }
-
-            route("/stats"){
-                route("/campaigns/{campaignId}") {
-                    get{
-                        StatisticsController(call).getCampaignStats()
-                    }
-                    get("/daily") {
-                        StatisticsController(call).getCampaignDailyStats()
-                    }
-                }
-                route("/advertisers/{advertiserId}/campaigns") {
-                    get {
-                        StatisticsController(call).getAdvertiserStats()
-                    }
-                    get("/daily") {
-                        StatisticsController(call).getAdvertiserDailyStats()
-                    }
-                }
-            }
-
-            route("/moderation"){
-                post("/addrestrictedwords"){
-                    ModerationController(call).addRestrictedWords()
-                }
-
-                post{
-                    ModerationController(call).switchRestrictions()
-                }
-            }
-
         }
+
+        post("/ml-scores") {
+            AdvertiserController(call).updateMlScore()
+        }
+
+        route("/ads") {
+            get {
+                CampaignController(call).getAd()
+            }
+            post("/{adId}/click") {
+                CampaignController(call).clickAd()
+            }
+        }
+
+        route("/stats") {
+            route("/campaigns/{campaignId}") {
+                get {
+                    StatisticsController(call).getCampaignStats()
+                }
+                get("/daily") {
+                    StatisticsController(call).getCampaignDailyStats()
+                }
+            }
+            route("/advertisers/{advertiserId}/campaigns") {
+                get {
+                    StatisticsController(call).getAdvertiserStats()
+                }
+                get("/daily") {
+                    StatisticsController(call).getAdvertiserDailyStats()
+                }
+            }
+        }
+
+        route("/moderation") {
+            post("/addrestrictedwords") {
+                ModerationController(call).addRestrictedWords()
+            }
+
+            post {
+                ModerationController(call).switchRestrictions()
+            }
+        }
+
     }
+
 }
